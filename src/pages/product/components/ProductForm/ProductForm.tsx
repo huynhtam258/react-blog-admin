22bd1872e22b6523ad259bad9cf069fa159e02c3
HuynhManhTam <huynhtam258@gmail.com>
2024-10-03 22:13:55 +0700

    fix: product form

--- src/pages/product/components/ProductForm/ProductForm.tsx
+++ src/pages/product/components/ProductForm/ProductForm.tsx
@@ -1,72 +1,82 @@
 import React, { useState } from 'react';
 import { useForm, SubmitHandler } from 'react-hook-form';
-import { Card, Input, Button, Typography } from '@material-tailwind/react';
+import { Card, Input, Button, Typography, Textarea, Select, Option } from '@material-tailwind/react';
 
 interface IFormInput {
   productName: string;
-  productDetails: string;
+  productDescription: string;
   productPrice: number;
+  productQuantity: number;
+  productType: string;
 }
 
+const productTypes = [
+  { value: 'Clothing', label: 'Clothing' },
+];
+
 function ProductForm() {
-  const [step, setStep] = useState(1);
-  const { register, handleSubmit, formState: { errors } } = useForm<IFormInput>();
+  const [formProductValues] = useState();
+  const { register, handleSubmit, setValue, formState: { errors, isValid } } = useForm<IFormInput>({
+    defaultValues: formProductValues,
+  });
 
   const onSubmit: SubmitHandler<IFormInput> = (data) => {
     console.log(data);
-    setStep(step + 1);
   };
 
   return (
     <div className="flex justify-center h-screen w-100">
       <Card color="transparent" shadow={false} className="w-full">
-        {step === 1 && (
-          <>
-            <Typography variant="h4" color="blue-gray" className="mb-4">
-              Tạo Sản Phẩm
-            </Typography>
-            <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-4">
-              <Input
-                size="lg"
-                label="Tên Sản Phẩm"
-                {...register('productName', { required: 'Tên sản phẩm là bắt buộc' })}
-                error={!!errors.productName}
-                crossOrigin=""
-              />
-              <Input
-                size="lg"
-                label="Chi Tiết Sản Phẩm"
-                {...register('productDetails', { required: 'Chi tiết sản phẩm là bắt buộc' })}
-                error={!!errors.productDetails}
-                crossOrigin=""
-              />
-              <Input
-                size="lg"
-                label="Giá Sản Phẩm"
-                type="number"
-                {...register('productPrice', { required: 'Giá sản phẩm là bắt buộc' })}
-                error={!!errors.productPrice}
-                crossOrigin=""
-              />
-              <Button type="submit" fullWidth>
-                OK
-              </Button>
-            </form>
-          </>
-        )}
-        {step === 2 && (
-          <>
-            <Typography variant="h4" color="blue-gray" className="mb-4">
-              Đăng Hình Ảnh Sản Phẩm
-            </Typography>
-            <form className="flex flex-col gap-4">
-              <Input size="lg" label="Hình Ảnh Sản Phẩm" type="file" crossOrigin="" />
-              <Button fullWidth>
-                Đăng
-              </Button>
-            </form>
-          </>
-        )}
+        <>
+          <Typography variant="h4" color="blue-gray" className="mb-4">
+            Tạo Sản Phẩm
+          </Typography>
+          <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-4">
+            <Input
+              size="lg"
+              label="Tên Sản Phẩm"
+              {...register('productName', { required: 'Tên sản phẩm là bắt buộc' })}
+              error={!!errors.productName}
+              crossOrigin=""
+            />
+            <Textarea
+              size="lg"
+              label="Chi Tiết Sản Phẩm"
+              {...register('productDescription', { required: 'Chi tiết sản phẩm là bắt buộc' })}
+              error={!!errors.productDescription}
+            />
+            <Input
+              size="lg"
+              label="Giá Sản Phẩm"
+              type="number"
+              {...register('productPrice', { required: 'Giá sản phẩm là bắt buộc' })}
+              error={!!errors.productPrice}
+              crossOrigin=""
+            />
+            <Input
+              size="lg"
+              label="Số lượng sản phẩm"
+              type="number"
+              {...register('productQuantity', { required: 'Số lượng sản phẩm là bắt buộc' })}
+              error={!!errors.productQuantity}
+              crossOrigin=""
+            />
+            <Select
+              label="Chọn loại sản phẩm"
+              {...register('productType', { required: 'Loại sản phẩm là bắt buộc' })}
+              onChange={(event) => setValue('productType', event || '')}
+            >
+              {productTypes.map((type) => (
+                <Option key={type.value} value={type.value}>
+                  {type.label}
+                </Option>
+              ))}
+            </Select>
+            <Button disabled={!isValid} type="submit" fullWidth>
+              OK
+            </Button>
+          </form>
+        </>
       </Card>
     </div>
   );
