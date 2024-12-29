06caa6950bc41e30ffa540f9794409dfd047fe12
HuynhManhTam <huynhtam258@gmail.com>
2024-12-29 12:36:47 +0700

    feat: update route

--- src/routers/index.tsx
+++ src/routers/index.tsx
@@ -1,46 +1,17 @@
-import { lazy } from 'react';
 import { RouteObject } from 'react-router-dom';
 import Guard from '../components/Guard';
 
-import UnauthenticateLayout from '../layouts/UnauthenticateLayout';
-import MainLayout from '../layouts/MainLayout';
+import authRoutes from './authRoutes';
+import mainRoutes from './mainRoutes';
 
-// Dynamic imports for pages
-const Auth = lazy(() => import('../pages/auth/auth'));
-const Home = lazy(() => import('../pages/home/components/Home'));
-const Products = lazy(() => import('../pages/product/components/Products'));
-const CreatePost = lazy(() => import('../pages/blog/components/CreatePost'));
-const CreateProduct = lazy(() => import('../pages/product/components/ProductForm/ProductForm'));
-const BlogDetail = lazy(() => import('../pages/blog/components/BlogDetail'));
-const MediaList = lazy(() => import('../pages/media/components/MediaList'));
-const MediaUpload = lazy(() => import('../pages/media/components/MediaUpload'));
 
 export const routes: RouteObject[] = [
   {
     path: '/',
     element: <Guard />,
     children: [
-      {
-        path: 'auth',
-        element: <UnauthenticateLayout />,
-        children: [
-          { path: 'login', element: <Auth /> },
-        ],
-      },
-      {
-        path: '/',
-        element: <MainLayout />,
-        children: [
-          { path: '/posts', element: <Home /> },
-          { path: '/products', element: <Products /> },
-          { path: '/create-blog', element: <CreatePost />},
-          { path: '/create-product', element: <CreateProduct />,},
-          { path: '/edit/:id', element: <CreatePost />,},
-          { path: '/blog/:id', element: <BlogDetail />,},
-          { path: '/media', element: <MediaList />,},
-          { path: '/upload-image', element: <MediaUpload />}
-        ],
-      },
+      ...authRoutes,
+      ...mainRoutes
     ],
   },
 
