5071ff06c85b7dc0ff6a85f84b20b5c0c6bea927
HuynhManhTam <huynhtam258@gmail.com>
2025-01-18 11:52:15 +0700

    feat: handle search for post list

--- src/pages/blog/components/PostList/PostList.tsx
+++ src/pages/blog/components/PostList/PostList.tsx
@@ -6,20 +6,24 @@ import { useEffect, useState } from 'react';
 import { deletePost, getPostList } from './../../blog.thunk';
 import { useNavigate } from 'react-router-dom';
 import { PAGINATION } from '../../../../enums';
+import { DebouncedInput } from '../../../../components/common/input';
 
 const PostList = () => {
   const postList = useSelector((state: RootState) => state.blog.postList);
+
   const [page] = useState<number>(PAGINATION.PAGE);
   const [itemsPerPage] = useState<number>(PAGINATION.ITEM_PER_PAGE);
+  const [searchText, setSearchText] = useState<string>('');
+
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
 
   useEffect(() => {
-    const promise = dispatch(getPostList({ page, items_per_page: itemsPerPage }));
+    const promise = dispatch(getPostList({ page, items_per_page: itemsPerPage, search: searchText }));
     return () => {
       promise.abort();
     };
-  }, [dispatch, page, itemsPerPage]); // Include 'page' and 'itemsPerPage' in the dependency array
+  }, [dispatch, page, itemsPerPage, searchText]); // Include 'page' and 'itemsPerPage' in the dependency array
 
   const handleDelete = (postId: number) => {
     dispatch(deletePost(postId));
@@ -27,7 +31,6 @@ const PostList = () => {
   };
 
   const handleEditingPost = (postId: number) => {
-    // dispatch(startEditingPost(postId));
     navigate(`/edit/${postId}`);
   };
 
@@ -39,44 +42,54 @@ const PostList = () => {
   const TABLE_HEAD = ['Tên bài viết', 'Hình ảnh', 'Tác giả', 'Ngày tạo', ''];
 
   return (
-    <Card className="h-full w-full overflow-auto">
-      <table className="w-full min-w-max table-auto text-left">
-        <thead>
-          <tr>
-            {TABLE_HEAD.map((head) => (
-              <th
-                key={head}
-                className="border-b border-blue-gray-100 bg-blue-gray-50 p-4"
-              >
-                <Typography
-                  variant="small"
-                  color="blue-gray"
-                  className="font-normal leading-none opacity-70"
+    <>
+      <div className='mb-4'>
+      <DebouncedInput
+          type="text"
+          placeholder="Tìm kiếm bài viết"
+          value={searchText}
+          onChange={(value: string) => setSearchText(value)}
+        />
+      </div>
+      <Card className="h-full w-full overflow-auto">
+        <table className="w-full min-w-max table-auto text-left">
+          <thead>
+            <tr>
+              {TABLE_HEAD.map((head) => (
+                <th
+                  key={head}
+                  className="border-b border-blue-gray-100 bg-blue-gray-50 p-4"
                 >
-                  {head}
-                </Typography>
-              </th>
-            ))}
-          </tr>
-        </thead>
-        <tbody>
-          {postList.map((post, index) => {
-            const isLast = index === postList.length - 1;
-            const classes = isLast ? 'p-4' : 'p-4 border-b border-blue-gray-50';
-            return (
-              <PostItem
-                post={post}
-                classes={classes}
-                handleDelete={handleDelete}
-                handleEditingPost={handleEditingPost}
-                handleReadMorePost={handleReadMorePost}
-                key={`postItem-${index}`}
-              />
-            );
-          })}
-        </tbody>
-      </table>
-    </Card>
+                  <Typography
+                    variant="small"
+                    color="blue-gray"
+                    className="font-normal leading-none opacity-70"
+                  >
+                    {head}
+                  </Typography>
+                </th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {postList.map((post, index) => {
+              const isLast = index === postList.length - 1;
+              const classes = isLast ? 'p-4' : 'p-4 border-b border-blue-gray-50';
+              return (
+                <PostItem
+                  post={post}
+                  classes={classes}
+                  handleDelete={handleDelete}
+                  handleEditingPost={handleEditingPost}
+                  handleReadMorePost={handleReadMorePost}
+                  key={`postItem-${index}`}
+                />
+              );
+            })}
+          </tbody>
+        </table>
+      </Card>
+    </>
   );
 }
 
