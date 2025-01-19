9de8f52ce07405873936d4d3be21703d05770a80
HuynhManhTam <huynhtam258@gmail.com>
2025-01-19 16:22:52 +0700

    update code

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { Suspense } from 'react';
 import { useRoutes } from 'react-router-dom';
 import routes from './routers';
+import SpinnerInitial from './components/common/spinner/SpinnerInitial';
 
 function App() {
   const elements = useRoutes(routes);
   return (
     <div className="App">
-      <Suspense fallback={<div>Loading...</div>}>
+      <Suspense fallback={<SpinnerInitial />}>
         {elements}
       </Suspense>
     </div>
--- src/layouts/MainLayout/MainLayout.tsx
+++ src/layouts/MainLayout/MainLayout.tsx
@@ -1,11 +1,35 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import SideBar from "../../components/sideBar/SideBar"
 
 // styles
 import './MainLayout.scss'
-import { Outlet } from "react-router-dom"
+import { Outlet, useNavigate } from "react-router-dom"
+import { useAppDispatch } from "../../store"
+import { getUserProfile } from "../../pages/user/user.thunk"
+import SpinnerInitial from "../../components/common/spinner/SpinnerInitial"
 
 const MainLayout: React.FC = () => {
+  const [initialize, setInitialize] = useState<boolean>(false)
+  const navigate = useNavigate();
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    const fetchUserProfile = async () => {
+      try {
+        await dispatch(getUserProfile()).unwrap();
+        setInitialize(true);
+      } catch (error) {
+        localStorage.clear();
+        navigate('/auth/login', { replace: true });
+      }
+    };
+
+    fetchUserProfile();
+  }, [dispatch]);
+
+  if (!initialize) {
+    return <SpinnerInitial />
+  }
   return (
     <div className="main-layout flex bg-blue-gray-50/50">
       <SideBar></SideBar>
